6584cd9d0660055b1b276f53864ca67cee5f77a7
Amelia Ostergaard <amelia.marie.ostergaard@gmail.com>
2022-12-02 18:02:00 -0700

    read data

--- day-1/index.js
+++ day-1/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, world!");
+const fs = require("fs");
+
+const buffer = fs.readFileSync("./data/input.txt");
+
+const stringData = buffer.toString("utf-8");
+
+const lines = stringData.split("\n");
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(`${i + 1}: ${lines[i]}`);
+}
