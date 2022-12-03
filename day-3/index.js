9e6f97f58e01e8dc19e56183ca9955689d143b8f
Amelia Ostergaard <amelia.marie.ostergaard@gmail.com>
2022-12-03 00:40:16 -0700

    solve day-3 part 2

--- day-3/index.js
+++ day-3/index.js
@@ -4,20 +4,28 @@ const buffer = fs.readFileSync("./data/input.txt");
 
 const stringData = buffer.toString("utf-8");
 
-const rucksacks = stringData
-  .trim()
-  .split("\n")
-  .map((rucksack) => {
-    const compartmentA = rucksack.slice(0, rucksack.length / 2).split("");
-    const compartmentB = rucksack.slice(rucksack.length / 2).split("");
+const rucksacks = stringData.trim().split("\n");
 
-    return [compartmentA, compartmentB];
-  });
+const compartments = rucksacks.map((rucksack) => {
+  const compartmentA = rucksack.slice(0, rucksack.length / 2).split("");
+  const compartmentB = rucksack.slice(rucksack.length / 2).split("");
 
-const duplicates = rucksacks.map(([compartmentA, compartmentB]) => {
-  const intersection = compartmentA.filter((item) =>
-    compartmentB.includes(item)
+  return [compartmentA, compartmentB];
+});
+
+function getIntersection(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const set2 = new Set(arr2);
+
+  const intersection = new Set(
+    [...set1].filter((element) => set2.has(element))
   );
+
+  return [...intersection];
+}
+
+const duplicates = compartments.map(([compartmentA, compartmentB]) => {
+  const intersection = getIntersection(compartmentA, compartmentB);
 
   // Assume all duplicates are same
   return intersection[0];
@@ -27,11 +35,40 @@ function isLowerCase(letter) {
   return letter === letter.toLowerCase();
 }
 
-const priorities = duplicates.map((duplicate) => {
-  const letterScore = duplicate.toLowerCase().charCodeAt(0) - 96;
-  return isLowerCase(duplicate) ? letterScore : letterScore + 26;
+function prioritizeItem(item) {
+  const letterScore = item.toLowerCase().charCodeAt(0) - 96;
+  return isLowerCase(item) ? letterScore : letterScore + 26;
+}
+
+const duplicatePriorities = duplicates.map(prioritizeItem);
+
+const duplicatePriorityTotal = duplicatePriorities.reduce(
+  (curr, val) => curr + val,
+  0
+);
+
+console.log("Part 1 (duplicates priority total):", duplicatePriorityTotal);
+
+function chunk(arr, chunkSize) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    result.push(chunk);
+  }
+  return result;
+}
+
+const groups = chunk(rucksacks, 3);
+const badges = groups.map(([rucksackA, rucksackB, rucksackC]) => {
+  const intersection = getIntersection(
+    rucksackA,
+    getIntersection(rucksackB, rucksackC)
+  );
+  return intersection[0];
 });
 
-const priorityTotal = priorities.reduce((curr, val) => curr + val, 0);
+const badgePriorities = badges.map(prioritizeItem);
 
-console.log("Part 1 (priority total):", priorityTotal);
+const badgePriorityTotal = badgePriorities.reduce((curr, val) => curr + val, 0);
+
+console.log("Part 2 (badge priority total):", badgePriorityTotal);
